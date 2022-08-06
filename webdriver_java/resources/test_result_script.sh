34d105156f1cfa1c3c168db5d5b921c8cd307187
Krishan <54745129+krish-r@users.noreply.github.com>
2022-08-06 16:23:30 +0530

    ci: add tags to github actions metrics

--- webdriver_java/resources/test_result_script.sh
+++ webdriver_java/resources/test_result_script.sh
@@ -15,10 +15,11 @@ TEST_RESULTS_STRING=$(cat "${TEST_RESULTS_LOCATION}/testng-results.xml" | grep "
 # echo "github_actions_failed_tests=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $8 }')" | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
 # echo "github_actions_skipped_tests=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')" | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
 
+# metric{key=value1, key2=value2} value
 cat <<EOF | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
-github_actions_ignored_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $2 }')
-github_actions_total_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $4 }')
-github_actions_passed_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $6 }')
-github_actions_failed_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $8 }')
-github_actions_skipped_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')
+github_actions_ignored_tests{commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $2 }')
+github_actions_total_tests{commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $4 }')
+github_actions_passed_tests{commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $6 }')
+github_actions_failed_tests{commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $8 }')
+github_actions_skipped_tests{commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')
 EOF
