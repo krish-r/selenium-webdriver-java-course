1dfe34ae462d28d6e324214af7a1ed2d18983f33
Krishan <54745129+krish-r@users.noreply.github.com>
2022-08-06 15:25:13 +0530

    fix: update test result script

--- webdriver_java/resources/test_result_script.sh
+++ webdriver_java/resources/test_result_script.sh
@@ -16,9 +16,9 @@ TEST_RESULTS_STRING=$(cat "${TEST_RESULTS_LOCATION}/testng-results.xml" | grep "
 # echo "github_actions_skipped_tests=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')" | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
 
 cat <<EOF | curl --data-binary @- ${PUSHGATEWAY_URL}/metrics/job/github_actions
-github_actions_ignored_tests=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $2 }')
-github_actions_total_tests=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $4 }')
-github_actions_passed_tests=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $6 }')
-github_actions_failed_tests=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $8 }')
-github_actions_skipped_tests=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')
+github_actions_ignored_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $2 }')
+github_actions_total_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $4 }')
+github_actions_passed_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $6 }')
+github_actions_failed_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $8 }')
+github_actions_skipped_tests $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')
 EOF
